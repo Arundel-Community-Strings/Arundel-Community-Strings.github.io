34ece8176b8ea63def4d80dbe4f9b6a670f19c8d
Ayris Narock <ayris.a.narock@gmail.com>
2018-09-06 23:03:01 +0000

    Meta tags for social media and content change requested by Chris

--- index.php
+++ index.php
@@ -4,19 +4,17 @@ $nav_focus = "home";
 include_once('header.php');
 ?>
 <div id="calloutlong">
-  <div id="calloutblock"><img class="calloutfloat" src="img/composing.jpg"/>
+  <div id="calloutblock">
+    <!-- <img class="calloutfloat" src="img/composing.jpg"/> -->
    <!-- width="358" height="104" alt="" /></a> -->
-    <h2>Hello Music Lovers!</h2>
-    <p>
-      We are Arundel Community Strings.  We hope you will join us.
-    </p>
-    <p>Registration for our fall session is now open.</p>
-    <p><a href="about.php">Learn more&#8230;</a></p>
-    <p><br/><br/>(Please pardon our dust as we are still composing this site. Pun intended!)</p>
+    <h1>
+      <br />
+    Fall Session Begins September 21st!
+    <p><a href="join.php"> Join Now</a></p>
+    </h1>
+
   </div>
-  <div style="width:1100px; text-align:right;">   
-	    <a target="_blank" href="https://www.facebook.com/AACSOMusic"><img alt="follow me on facebook" src="img/facebook-1.jpg" border=0></a>
-  </div>
+
   <!--// end #calloutblock //-->
 </div>
 <!--// end #callout //-->
--- outreach.php
+++ outreach.php
@@ -4,9 +4,9 @@ $nav_focus = "outreach";
 include_once('header.php');
 ?>
 
-<div id="callout">
+<div id="calloutlong">
   <div id="calloutblock">
-    <h2>Help us spread the word by hanging or passing out our flyers</h2>
+    <h2>Help us spread the word by hanging or passing out our flyers
     <br/><p> 
       <ul>
         <li><a href="docs/Flyer_Aug18.pdf">Handout Flyer</a>: No pull tabs</li>
@@ -17,7 +17,7 @@ include_once('header.php');
     <p>Tip: Keep a few flyers and some pushpins on you or in your car in case you happen across an 
       unexpected, great place to hang one!
     </p>
-
+    </h2>
   </div>
   <!--// end #calloutblock //-->
 </div>
