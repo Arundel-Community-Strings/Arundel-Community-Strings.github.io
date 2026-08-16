dbcff87fbb965fe83c445268893cdf48dec462b5
Ayris Narock <ayris.a.narock@gmail.com>
2026-08-16 18:37:47 -0400

    Front page redo with astro and uswds

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+#
+# Script to build and deploy the ACS site to the docs/ folder
+#
+
+# Build CSS
+cd ./design
+npx gulp compile
+cp -r -p ./assets ../grubby-galaxy/public/.
+
+# Build Astro pages
+cd ../grubby-galaxy
+npm run build
+cd ..
+
+# Clear old and copy Astro dist to docs/
+rm -rf docs/*
+cp -r -p ./grubby-galaxy/dist/* ./docs/
--- docs/js/functions.js
+++
@@ -1,5 +0,0 @@
-function gen_mail_to_link(lhs,rhs) {
-	document.write("<a href=\"mailto");
-	document.write(":" + lhs + "@" + rhs);
-	document.write("\">" + lhs + "@" + rhs + "<\/a>");
-}
